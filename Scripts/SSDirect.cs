7d0acbdf8c4e0a0deb03111f8784214ed661bfc8
anubisxiang <54629158+anubisxiang@users.noreply.github.com>
2021-10-09 21:41:28 +0800

    Add files via upload

---
+++ Scripts/SSDirect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using ControllerApplication;
+using InterfaceApplication;
+
+public class SSDirect : System.Object{
+    private static SSDirect instance;
+    public ISceneController CurrentSceneController{ get; set;}   
+
+    public static SSDirect getInstance(){
+        if(instance==null) instance=new SSDirect();
+        return instance;
+    }
+}
