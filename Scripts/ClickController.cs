e66c48177faa12fc2b500c20c680d7865fc9aeed
anubisxiang <54629158+anubisxiang@users.noreply.github.com>
2021-10-09 21:39:16 +0800

    Add files via upload

---
+++ Scripts/ClickController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using ControllerApplication;
+using InterfaceApplication;
+   
+public class Click : MonoBehaviour
+{
+    IUserAction action;
+    RoleModel role=null;
+    BoatModel boat=null;
+    public void setRole(RoleModel role){
+        this.role=role;
+    }
+    public void setBoat(BoatModel boat){
+        this.boat=boat;
+    }
+
+    void Start(){
+        action=SSDirect.getInstance().CurrentSceneController as IUserAction;
+    }
+    void OnMouseDown(){
+        if(boat==null && role==null) return;
+
+        if(boat!=null) action.moveBoat();
+        else if(role!=null) action.moveRole(role);
+    }
+}
