73947903b0a8c74e7a01efefa525673d5eaea8a1
anubisxiang <54629158+anubisxiang@users.noreply.github.com>
2021-10-09 21:40:06 +0800

    Add files via upload

---
+++ Scripts/Interfaces.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using ControllerApplication;
+
+namespace  InterfaceApplication{
+    //场景接口   
+    public interface ISceneController{
+        void LoadResoureces();
+    }
+
+    //用户动作接口
+    public interface IUserAction{
+        void moveBoat();
+        void moveRole(RoleModel role);
+        void reStart();
+        int check();
+    }
+}
